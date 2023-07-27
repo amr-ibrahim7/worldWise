f5c92a18f44e2eaeca73c40ddcb101bb9fce8a01
amr-ibrahim7 <amrnew100@gmail.com>
2023-07-27 21:29:15 +0300

    adding advanced pattern A custom Provider and Hook

--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -1,9 +1,11 @@
+import { useCities } from "../contexts/CitiesContext";
 import CountryItem from "./CountryItem";
 import styles from "./CountryList.module.css";
 import Message from "./Message";
 import Spinner from "./Spinner";
 
-function CountriesList({ cities, isLoading }) {
+function CountriesList() {
+  const { cities, isLoading } = useCities();
   if (isLoading) return <Spinner />;
   if (!cities.length)
     return (
@@ -13,13 +15,16 @@ function CountriesList({ cities, isLoading }) {
   const countries = cities.reduce((arr, city) => {
     if (!arr.map((el) => el.country).includes(city.country))
       return [...arr, { country: city.country, emoji: city.emoji }];
+    // If the current city.country is not already in the arr (meaning it's a new country entry), the code creates a new object with the country and emoji properties taken from the current city. It then adds this new object to the arr using the spread operator [...arr].
     else return arr;
+    // If the city.country is already present in the arr (meaning it's a duplicate country entry), the code does not add anything to the arr.
   }, []);
+  // In summary, the code takes an array of cities, and from that, it creates a new array containing unique country entries along with their respective emojis. The code filters out duplicate country entries, ensuring that each country appears only once in the countries array.
 
   return (
     <ul className={styles.countryList}>
       {countries.map((country) => (
-        <CountryItem country={country} />
+        <CountryItem country={country} key={country.country} />
       ))}
     </ul>
   );
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,6 +1,27 @@
+import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
 function Map() {
-  return <div className={styles.mapContainer}>Mapp</div>;
+  const navigate = useNavigate();
+
+  const [searchParams, setSearchParams] = useSearchParams();
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+
+  return (
+    <div className={styles.mapContainer} onClick={() => navigate("form")}>
+      <h1> Mapp</h1>
+      <h2>
+        Position : {lat}, {lng}
+      </h2>
+      <button
+        onClick={() => {
+          setSearchParams({ lat: 50, lng: 100 });
+        }}
+      >
+        Change Position
+      </button>
+    </div>
+  );
 }
 
 export default Map;
